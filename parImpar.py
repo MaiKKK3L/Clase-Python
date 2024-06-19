cea0218c18fc43966c339a21995c9a029c613e6d
MaiKKK3L <maik.toledo@duocuc.cl>
2024-06-19 11:01:15 -0400

    Add files via upload

---
+++ parImpar.py
@@ -0,0 +1,14 @@
+lista_num =[2,3,7,12,16,27]
+listaPar = []
+listaImpar = []
+def num_parImpar():
+    for i in lista_num:
+        if i % 2 == 0:
+            listaPar.append
+        else:
+            listaImpar.append
+            
+print("Numeros Pares:", listaPar)
+print("Numeros Impares:", listaImpar)
+
+num_parImpar()
