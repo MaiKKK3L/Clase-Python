d49b946ee9bb359336f1bb6661791b013d6c8a52
MaiKKK3L <maik.toledo@duocuc.cl>
2024-06-19 11:20:01 -0400

    Add files via upload
    Esta es la correción

--- parImpar.py
+++ parImpar.py
@@ -2,13 +2,13 @@ lista_num =[2,3,7,12,16,27]
 listaPar = []
 listaImpar = []
 def num_parImpar():
-    for i in lista_num:
+    for i in range (len (lista_num)):
         if i % 2 == 0:
-            listaPar.append
+            listaPar.append(i)
         else:
-            listaImpar.append
+            listaImpar.append(i)
             
-print("Numeros Pares:", listaPar)
-print("Numeros Impares:", listaImpar)
+    print(f"Numeros Pares:", listaPar)
+    print(f"Numeros Impares:", listaImpar)
 
 num_parImpar()
